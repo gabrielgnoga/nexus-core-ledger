113dc1d76440ea9de314e682e55c0a3b032c14ad
Gabriel Noga <gabriel.gnoga@gmail.com>
2026-01-05 19:28:13 -0300

    legal: add MIT license and project metadata in pom.xml

---
+++ nexus-core-ledger/src/main/java/io/github/gabrielgnoga/nexus_core_ledger/domain/model/AccountType.java
@@ -0,0 +1,13 @@
+package io.github.gabrielgnoga.nexus_core_ledger.domain.model;
+
+public enum AccountType {
+    ASSET,      // Ativos
+    LIABILITY,  // Passivos
+    EQUITY,     // Patrimônio líquido
+    REVENUE,    // Receita
+    EXPENSE     // Despesa
+}
+
+
+
+
