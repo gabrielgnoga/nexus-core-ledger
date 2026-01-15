80195feef03373bffaa5ab7eb29f789fa314084c
Gabriel Noga <gabriel.gnoga@gmail.com>
2026-01-14 21:55:04 -0300

    feat: implement account controller and refactor service with builder pattern

--- src/main/java/io/github/gabrielgnoga/nexus_core_ledger/service/AccountService.java
+++ src/main/java/io/github/gabrielgnoga/nexus_core_ledger/service/AccountService.java
@@ -22,19 +22,13 @@ public class AccountService {
             throw new IllegalArgumentException("Já existe uma conta com o nome: " + dto.getName());
         }
 
-        // 2. Montagem da Entidade (Mapping)
-        // Aqui transformamos o DTO (papelada) em uma Entidade Real (objeto do banco)
-        Account newAccount = new Account();
-        newAccount.setName(dto.getName());
+        Account newAccount = Account.builder()
+                .name(dto.getName())
+                .accountType(AccountType.valueOf(dto.getAccountType().toUpperCase()))
+                .balance(BigDecimal.ZERO) // balance starts on zero
+                .currency("BRL")          // setting a pattern currency
+                .build();                 // creates the final object and closes its package
 
-        // Convertendo a String do DTO para o Enum AccountType
-        // Se o usuário mandar algo errado, o Java estoura erro automaticamente aqui
-        newAccount.setAccountType(AccountType.valueOf(dto.getAccountType().toUpperCase()));
-
-        // Regra definida: Toda conta nasce com saldo 0
-        newAccount.setBalance(BigDecimal.ZERO);
-
-        // 3. Salvar no Banco
         return accountRepository.save(newAccount);
     }
 }
