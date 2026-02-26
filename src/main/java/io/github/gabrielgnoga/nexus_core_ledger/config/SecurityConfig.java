f00a4b9f35f402a14267136df770297a58c68c94
Gabriel Noga <gabriel.gnoga@gmail.com>
2026-02-26 14:31:11 -0300

    "feat: add spring security, configure stateless session and secure endpoints"

---
+++ src/main/java/io/github/gabrielgnoga/nexus_core_ledger/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package io.github.gabrielgnoga.nexus_core_ledger.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Classe de configuração central do Spring Security.
+ *
+ * <p>Define as regras de quem entra, quem sai e como a autenticação é feita
+ * na API, desativando formulários de login padrão e configurando a segurança
+ * para o modo Stateless (ideal para uso com Tokens JWT).</p>
+ *
+ * @author Gabriel Gnoga
+ * @since 1.0.0
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
