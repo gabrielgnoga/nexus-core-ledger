14cab07cfc58ecc4ce5bb31ba548da8cff71eb87
Gabriel Noga <gabriel.gnoga@gmail.com>
2026-03-11 21:27:31 -0300

    "feat: implement SecurityFilter for JWT token validation and request interception"

---
+++ src/main/java/io/github/gabrielgnoga/nexus_core_ledger/config/SecurityFilter.java
@@ -0,0 +1,58 @@
+package io.github.gabrielgnoga.nexus_core_ledger.config;
+
+import io.github.gabrielgnoga.nexus_core_ledger.repository.UserRepository;
+import io.github.gabrielgnoga.nexus_core_ledger.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+/**
+ * Filtro que intercepta todas as requisições para validar o Token JWT.
+ * * @author Gabriel Gnoga
+ * @since 1.0.0
+ */
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+
+        if (token != null) {
+            var login = tokenService.validateToken(token);
+
+            UserDetails user = userRepository.findByLogin(login);
+
+            if (user != null) {
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+
+        // O padrão da web é enviar o token escrito "Bearer meutokengigante..."
+        // Então nós cortamos a palavra "Bearer " para ficar só com o código puro.
+        return authHeader.replace("Bearer ", "");
+    }
+}
--- src/main/java/io/github/gabrielgnoga/nexus_core_ledger/service/TokenService.java
+++ src/main/java/io/github/gabrielgnoga/nexus_core_ledger/service/TokenService.java
@@ -77,4 +77,5 @@ public class TokenService {
     private Instant genExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
+
 }
