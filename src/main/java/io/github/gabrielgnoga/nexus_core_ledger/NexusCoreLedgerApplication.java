407b5586a35db4dce28f44414123aee13231f3e0
Gabriel Noga <gabriel.gnoga@gmail.com>
2026-03-13 14:51:36 -0300

    "feat: add TestController for security testing and configure Swagger OpenAPI security scheme"

--- src/main/java/io/github/gabrielgnoga/nexus_core_ledger/NexusCoreLedgerApplication.java
+++ src/main/java/io/github/gabrielgnoga/nexus_core_ledger/NexusCoreLedgerApplication.java
@@ -1,8 +1,23 @@
 package io.github.gabrielgnoga.nexus_core_ledger;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(title = "Minha API", version = "1.0.0"),
+		security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT"
+)
 @SpringBootApplication
 public class NexusCoreLedgerApplication {
 
---
+++ src/main/java/io/github/gabrielgnoga/nexus_core_ledger/controller/TestController.java
@@ -0,0 +1,21 @@
+package io.github.gabrielgnoga.nexus_core_ledger.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller de teste para validar a segurança das rotas (A nossa "Sala Cofre").
+ * * @author Gabriel Gnoga
+ * @since 1.0.0
+ */
+@RestController
+@RequestMapping("/v1/test")
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> secretRoute() {
+        return ResponseEntity.ok("Acesso Liberado! Você está na área VIP da API e seu crachá é válido! ");
+    }
+}
